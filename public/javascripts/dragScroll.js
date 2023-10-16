5f011c5aeff4c0edd02204257b2e73b20371319e
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2023-10-16 10:54:15 +0600

    Fix: dragScroll

--- public/javascripts/dragScroll.js
+++ public/javascripts/dragScroll.js
@@ -1,45 +1,47 @@
-function clickAndDrag(
-  selector,
-  scroll_speed = 1,
-  classOnEvent = "grabbed_elem"
-) {
-  const sliders = document.querySelectorAll(selector);
-  sliders.forEach((slider) => {
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+/* 
+  this is an implementation of Wes Bos click & drag scroll algorythm. In his video, he shows how to do the horizontal scroll. I have implemented the vertical scroll for those wondering how to make it as well.
+  
+  Wes Bos video:
+    https://www.youtube.com/watch?v=C9EWifQ5xqA
+*/
+const containers = document.querySelectorAll(".parent");
+containers.forEach((container) => {
+  let startY;
+  let startX;
+  let scrollLeft;
+  let scrollTop;
+  let isDown;
 
-    slider.addEventListener("mousedown", (e) => {
+  container.addEventListener("mousedown", (e) => mouseIsDown(e));
+  container.addEventListener("mouseup", (e) => mouseUp(e));
+  container.addEventListener("mouseleave", (e) => mouseLeave(e));
+  container.addEventListener("mousemove", (e) => mouseMove(e));
+
+  function mouseIsDown(e) {
+    isDown = true;
+    startY = e.pageY - container.offsetTop;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+    scrollTop = container.scrollTop;
+  }
+  function mouseUp(e) {
+    isDown = false;
+  }
+  function mouseLeave(e) {
+    isDown = false;
+  }
+  function mouseMove(e) {
+    if (isDown) {
       e.preventDefault();
-      isDown = true;
-      slider.classList.add(classOnEvent);
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
+      //Move vertcally
+      const y = e.pageY - container.offsetTop;
+      const walkY = y - startY;
+      container.scrollTop = scrollTop - walkY;
 
-      // prevent default child behavior
-      document.body.addEventListener("click", function (event) {
-        if (slider.contains(event.target)) {
-          event.preventDefault();
-        }
-      });
-    });
-    slider.addEventListener("mouseleave", () => {
-      isDown = false;
-      slider.classList.remove(classOnEvent);
-    });
-    slider.addEventListener("mouseup", () => {
-      isDown = false;
-      slider.classList.remove(classOnEvent);
-    });
-    slider.addEventListener("mousemove", (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = (x - startX) * scroll_speed; //scroll-fast
-      slider.scrollLeft = scrollLeft - walk;
-    });
-  });
-}
-
-// usage
-clickAndDrag(".parent");
+      //Move Horizontally
+      const x = e.pageX - container.offsetLeft;
+      const walkX = x - startX;
+      container.scrollLeft = scrollLeft - walkX;
+    }
+  }
+});
